e9e54c776a83f1880d3df4e842546f49417c2e9c
rodionboroviy <rodionboroviy@gmail.com>
2018-07-18 22:52:02 +0300

    added program files

---
+++ blockchain/block.test.js
@@ -0,0 +1,33 @@
+const Block = require('./block');
+const { DIFFiCULTY } = require('../config');
+
+describe('Block', () => {
+    let data, lastHash, block;
+
+    beforeEach(() => {
+        data = 'bar';
+        lastBlock = Block.genesis();
+        block = Block.mineBlock(lastBlock, data);
+    });
+
+    it('sets the `data` to match the input', () => {
+        expect(block.data).toEqual(data);
+    });
+
+    it('sets the `lastHash` to match the hash of the last block', () => {
+        expect(block.lastHash).toEqual(lastBlock.hash);
+    });
+
+    it('generates a hash that matches the difficulty', () => {
+        expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty));
+        console.log(block.toString());
+    });
+
+    it('lowers the difficulty for slowly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp + 360000)).toEqual(block.difficulty - 1);
+    });
+
+    it('raises the difficulty for slowly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp + 1)).toEqual(block.difficulty + 1);
+    });
+});
